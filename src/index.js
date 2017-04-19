53b8887b5f6e65e35a81eb61e952699adae3e551
Stig Alvestad <stig@telemagic.no>
2017-04-19 22:39:35 +0200

    Use Provider from react-redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { createStore } from 'redux';
 import reducer from './reducers';
-
-class Provider extends Component {
-  getChildContext() {
-    return {
-      store: this.props.store
-    };
-  }
-
-  render() {
-    return this.props.children;
-  }
-}
-Provider.childContextTypes = {
-  store: React.PropTypes.object
-};
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <Provider store={createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
